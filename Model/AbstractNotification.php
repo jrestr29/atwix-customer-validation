4db8c6e1c0013b63d9d3d5f403bd7d15f1a22420
Jose Restrepo <josda6486@gmail.com>
2023-06-08 00:08:42 -0500

    Include base config on abstract model

--- Model/AbstractNotification.php
+++ Model/AbstractNotification.php
@@ -6,9 +6,24 @@ declare(strict_types=1);
 
 namespace Atwix\CustomerValidation\Model;
 
+use Atwix\CustomerValidation\Model\Config as ConfigModel;
 use Magento\Customer\Api\Data\CustomerInterface;
 abstract class AbstractNotification
 {
+    /**
+     * @var Config
+     */
+    protected $_config;
+
+    /**
+     * @param Config $config
+     */
+    public function __construct(
+        ConfigModel $config
+    ) {
+        $this->_config = $config;
+    }
+
     /**
      * @param CustomerInterface $customer
      * @return mixed
