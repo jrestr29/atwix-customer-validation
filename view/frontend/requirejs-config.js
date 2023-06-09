063f1072c897d3756d7c30270ed349ebf98f564d
Jose Restrepo <josda6486@gmail.com>
2023-06-08 23:09:54 -0500

    Create custom extended validation for firstname

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'Atwix_CustomerValidation/js/validate-customer-firstname': true
+            }
+        }
+    }
+}
